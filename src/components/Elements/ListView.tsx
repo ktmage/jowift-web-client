46d1ef7c30e91deb643904109d912bcab6ac1420
natsuki <yasashisainternet@gmail.com>
2023-12-23 21:23:55 +0900

    [update]ListView.tsx
    ListViewのデザインを更新しました。

--- src/components/Elements/ListView.tsx
+++ src/components/Elements/ListView.tsx
@@ -1,3 +1,6 @@
+import { Box, List, ListItemButton } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 interface ListViewItem {
 	text: string;
 	to: string;
@@ -9,10 +12,27 @@ interface ListViewProps {
 
 export default function ListView(props: ListViewProps) {
 	return (
-		<ul>
-			{props.items.map((item) => (
-				<li key={item.text}>{item.text}</li>
-			))}
-		</ul>
+		<Box
+			bgcolor={'background.paper'}
+			height={'100%'}
+			display={'flex'}
+			flexDirection={'column'}
+			overflow={'visible'}
+			sx={{
+				overflowX: 'hidden',
+			}}
+		>
+			<List>
+				{props.items.map((item, index) => (
+					<ListItemButton
+						key={index}
+						component={Link}
+						to={item.to}
+					>
+						{item.text}
+					</ListItemButton>
+				))}
+			</List>
+		</Box>
 	);
 }
