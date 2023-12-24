b38f0c513340b5a55d4b86e9cd03ee72a45e2686
natsuki <yasashisainternet@gmail.com>
2023-12-24 22:07:30 +0900

    [update]FormLayout.tsx
    FormLayoutのデザインを作成しました。

--- src/components/Layouts/FormLayout/index.tsx
+++ src/components/Layouts/FormLayout/index.tsx
@@ -1,7 +1,52 @@
+import { Box, IconButton, Stack } from '@mui/material';
+
+interface headerItems {
+	icon: React.ReactNode;
+	onClick: () => void;
+}
+
 interface FormLayoutProps {
 	children: React.ReactNode;
+	headerItems: headerItems[];
+	disableHeader?: boolean;
 }
 
 export default function FormLayout(props: FormLayoutProps) {
-	return <>{props.children}</>;
+	const { children, disableHeader = false, headerItems } = props;
+
+	return (
+		<Box
+			display={'flex'}
+			flexDirection={'column'}
+			height={'100%'}
+		>
+			<Stack
+				display={disableHeader ? 'none' : 'flex'}
+				direction={'row'}
+				justifyContent={'flex-end'}
+				paddingX={2}
+			>
+				{headerItems.map((item, index) => (
+					<IconButton
+						key={index}
+						onClick={item.onClick}
+					>
+						{item.icon}
+					</IconButton>
+				))}
+			</Stack>
+
+			<Stack
+				spacing={4}
+				paddingX={'15%'}
+				paddingY={8}
+				flexGrow={1}
+				sx={{
+					overflowY: 'auto',
+				}}
+			>
+				{children}
+			</Stack>
+		</Box>
+	);
 }
