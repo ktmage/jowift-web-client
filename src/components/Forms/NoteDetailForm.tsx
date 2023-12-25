b040ca78ca9e4bb8d2a15ce5afd2f09139082293
natsuki <yasashisainternet@gmail.com>
2023-12-25 22:52:36 +0900

    [update] NoteDetailForm.tsxにフォームの変更を判定する処理を追加
    変更されていない場合は、更新ボタンを押せないようにする。

--- src/components/Forms/NoteDetailForm.tsx
+++ src/components/Forms/NoteDetailForm.tsx
@@ -3,21 +3,40 @@ import SaveIcon from '@mui/icons-material/Save';
 import LockIcon from '@mui/icons-material/Lock';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import { Autocomplete, TextField } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function NoteDetailForm() {
-	const [title, setTitle] = useState<string>('');
-	const [tags, setTags] = useState<string[]>([]);
-	const [content, setContent] = useState<string>('');
-	const tagOptions = ['tag1', 'tag2', 'tag3'];
+	// 仮のタグデータ
+	const mockTags = ['tag1', 'tag2', 'tag3'];
+
+	// 仮のノートデータ
+	const mockNote = {
+		title: 'テストタイトル',
+		tags: [mockTags[0], mockTags[2]],
+		content: 'テストコンテンツ',
+	};
+
+	const [title, setTitle] = useState<string>(mockNote.title);
+	const [tags, setTags] = useState<string[]>(mockNote.tags);
+	const [content, setContent] = useState<string>(mockNote.content);
 
 	const [isLocked, setIsLocked] = useState<boolean>(true);
+	const [isChanged, setIsChanged] = useState<boolean>(false);
+
+	// 参照元のデータと比較して変更があったかどうかを判定、変更があった場合はフラグを立てる。
+	useEffect(() => {
+		const tagsChanged = JSON.stringify(mockNote.tags) !== JSON.stringify(tags);
+		const titleChanged = mockNote.title !== title;
+		const contentChanged = mockNote.content !== content;
+		setIsChanged(tagsChanged || titleChanged || contentChanged);
+	}, [title, tags, content, mockNote.title, mockNote.tags, mockNote.content]);
+
 	return (
 		<FormLayout
 			headerItems={[
 				{
 					icon: <SaveIcon />,
-					onClick: () => console.log('save'),
+					onClick: () => (isChanged ? console.log('save') : console.log('no change')),
 				},
 				{
 					icon: isLocked ? <LockIcon /> : <LockOpenIcon />,
@@ -37,7 +56,7 @@ export default function NoteDetailForm() {
 			<Autocomplete
 				readOnly={isLocked}
 				multiple
-				options={tagOptions}
+				options={mockTags}
 				value={tags}
 				onChange={(e, value) => setTags(value)}
 				renderInput={(params) => (
