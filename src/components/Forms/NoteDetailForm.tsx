8b898cf09c21fc5cdd64fcb1bf76517bd92665fd
natsuki <yasashisainternet@gmail.com>
2023-12-25 22:02:19 +0900

    [update] NoteDetailForm.tsx
    TextFieldを追加、基本的な表示はできるようになった。

--- src/components/Forms/NoteDetailForm.tsx
+++ src/components/Forms/NoteDetailForm.tsx
@@ -1,8 +1,14 @@
 import { FormLayout } from '../Layouts';
 import SaveIcon from '@mui/icons-material/Save';
 import LockIcon from '@mui/icons-material/Lock';
+import { Autocomplete, TextField } from '@mui/material';
+import { useState } from 'react';
 
 export default function NoteDetailForm() {
+	const [title, setTitle] = useState<string>('');
+	const [tags, setTags] = useState<string[]>([]);
+	const [content, setContent] = useState<string>('');
+	const tagOptions = ['tag1', 'tag2', 'tag3'];
 	return (
 		<FormLayout
 			headerItems={[
@@ -16,7 +22,31 @@ export default function NoteDetailForm() {
 				},
 			]}
 		>
-			<>note detail</>
+			<TextField
+				inputProps={{ sx: { fontSize: '1.5rem', fontWeight: 'bold' } }}
+				value={title}
+				placeholder='タイトル'
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+			<Autocomplete
+				multiple
+				options={tagOptions}
+				value={tags}
+				onChange={(e, value) => setTags(value)}
+				renderInput={(params) => (
+					<TextField
+						{...params}
+						placeholder='タグ'
+						size='small'
+					/>
+				)}
+			/>
+			<TextField
+				multiline
+				value={content}
+				placeholder='内容'
+				onChange={(e) => setContent(e.target.value)}
+			/>
 		</FormLayout>
 	);
 }
