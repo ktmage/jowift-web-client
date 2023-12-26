e87847f71cda02f5799b65fd4f0410f74462a74c
natsuki <yasashisainternet@gmail.com>
2023-12-26 18:26:14 +0900

    [update] TagDetailForm.tsxの編集ロックを実装
    編集をロックするために、ロック状態を管理するstateを追加しました。

--- src/components/Forms/TagDetailForm.tsx
+++ src/components/Forms/TagDetailForm.tsx
@@ -1,6 +1,7 @@
 import { FormLayout } from '../Layouts';
 import SaveIcon from '@mui/icons-material/Save';
 import LockIcon from '@mui/icons-material/Lock';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 import AutorenewIcon from '@mui/icons-material/Autorenew';
 import { TextField } from '@mui/material';
 import { useState } from 'react';
@@ -11,6 +12,10 @@ export default function TagDetailForm() {
 	};
 
 	const [name, setName] = useState<string>(mockTag.name);
+
+	// 編集ロックの状態を管理
+	const [isLocked, setIsLocked] = useState<boolean>(true);
+
 	return (
 		<FormLayout
 			headerItems={[
@@ -23,13 +28,16 @@ export default function TagDetailForm() {
 					onClick: () => {},
 				},
 				{
-					icon: <LockIcon />,
-					onClick: () => {},
+					icon: isLocked ? <LockIcon /> : <LockOpenIcon />,
+					onClick: () => setIsLocked(!isLocked),
 				},
 			]}
 		>
 			<TextField
-				inputProps={{ sx: { fontSize: '1.5rem', fontWeight: 'bold' } }}
+				inputProps={{
+					sx: { fontSize: '1.5rem', fontWeight: 'bold' },
+					readOnly: isLocked,
+				}}
 				value={name}
 				placeholder='タグの名前'
 				onChange={(e) => setName(e.target.value)}
