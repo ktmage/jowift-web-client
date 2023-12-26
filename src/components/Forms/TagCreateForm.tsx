e26860f072da28a40f80bb5ff80eb435faa397db
natsuki <yasashisainternet@gmail.com>
2023-12-26 18:04:27 +0900

    [update] TagCreateForm.tsxにテキストフィールドを追加
    タグの名前を入力するテキストフィールドを追加しました。

--- src/components/Forms/TagCreateForm.tsx
+++ src/components/Forms/TagCreateForm.tsx
@@ -1,7 +1,10 @@
+import { TextField } from '@mui/material';
 import { FormLayout } from '../Layouts';
 import SaveIcon from '@mui/icons-material/Save';
+import { useState } from 'react';
 
 export default function TagCreateForm() {
+	const [name, setName] = useState<string>('');
 	return (
 		<FormLayout
 			headerItems={[
@@ -11,7 +14,12 @@ export default function TagCreateForm() {
 				},
 			]}
 		>
-			<h1>Create Tag</h1>
+			<TextField
+				inputProps={{ sx: { fontSize: '1.5rem', fontWeight: 'bold' } }}
+				value={name}
+				placeholder='タグの名前'
+				onChange={(e) => setName(e.target.value)}
+			/>
 		</FormLayout>
 	);
 }
