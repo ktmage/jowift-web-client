d0f19dfc0f13290b1d4fceac9dfc97f51450e8c3
natsuki <yasashisainternet@gmail.com>
2023-12-26 18:00:06 +0900

    [update] TagCreateForm.tsxのHeaderに保存ボタンを追加
    タグを保存するためのボタンを追加しました。

--- src/components/Forms/TagCreateForm.tsx
+++ src/components/Forms/TagCreateForm.tsx
@@ -1,7 +1,17 @@
+import { FormLayout } from '../Layouts';
+import SaveIcon from '@mui/icons-material/Save';
+
 export default function TagCreateForm() {
 	return (
-		<div>
-			<h1>TagCreateForm</h1>
-		</div>
+		<FormLayout
+			headerItems={[
+				{
+					icon: <SaveIcon />,
+					onClick: () => console.log('save'),
+				},
+			]}
+		>
+			<h1>Create Tag</h1>
+		</FormLayout>
 	);
 }
