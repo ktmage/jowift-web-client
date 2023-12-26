1e97bc30c4fd8280655b11c7d697bf2787505427
natsuki <yasashisainternet@gmail.com>
2023-12-26 18:09:39 +0900

    [add] TagDetailForm.tsx
    ファイルを作成しました。

---
+++ src/components/Forms/TagDetailForm.tsx
@@ -0,0 +1,3 @@
+export default function TagDetailForm() {
+	return <div>TagDetailForm</div>;
+}
--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -1,3 +1,4 @@
 export { default as NoteCreateForm } from './NoteCreateForm';
 export { default as NoteDetailForm } from './NoteDetailForm';
 export { default as TagCreateForm } from './TagCreateForm';
+export { default as TagDetailForm } from './TagDetailForm';
