0f9a6d6b6f04ebab88a51f126be6f11ed733dd6d
natsuki <yasashisainternet@gmail.com>
2023-12-26 18:16:18 +0900

    [update] TagDetailForm.tsx
    FormLayoutにアイコンを追加

--- src/components/Forms/TagDetailForm.tsx
+++ src/components/Forms/TagDetailForm.tsx
@@ -1,3 +1,27 @@
+import { FormLayout } from '../Layouts';
+import SaveIcon from '@mui/icons-material/Save';
+import LockIcon from '@mui/icons-material/Lock';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+
 export default function TagDetailForm() {
-	return <div>TagDetailForm</div>;
+	return (
+		<FormLayout
+			headerItems={[
+				{
+					icon: <SaveIcon />,
+					onClick: () => {},
+				},
+				{
+					icon: <AutorenewIcon />,
+					onClick: () => {},
+				},
+				{
+					icon: <LockIcon />,
+					onClick: () => {},
+				},
+			]}
+		>
+			<div>TagDetailForm</div>
+		</FormLayout>
+	);
 }
