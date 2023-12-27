3737d623df6fa64fcc5973ab047aab826601ff35
natsuki <yasashisainternet@gmail.com>
2023-12-27 21:43:44 +0900

    [update] routes/indexの変更。
    useAuthを使って認証の状態を取得し、ルーティングを変更するようにしました。

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
 import { useRoutes } from 'react-router-dom';
 import protectedRoutes from './protected';
-// import publicRoutes from './public';
+import publicRoutes from './public';
+import { useAuth } from '@/hooks';
 
 export default function AppRoutes() {
-	const routes = useRoutes(protectedRoutes);
-	// const routes = useRoutes(publicRoutes);
+	// 認証機能の呼び出し
+	const { isAuth } = useAuth();
+
+	// 認証されていたらprotectedRoutesを、されていなかったらpublicRoutesを表示する。
+	const routes = useRoutes(isAuth ? protectedRoutes : publicRoutes);
 	return <>{routes}</>;
 }
