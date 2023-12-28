344adae46a309ebfbad8dc30c92fb7f7b3adb137
natsuki <yasashisainternet@gmail.com>
2023-12-28 12:40:14 +0900

    [update] routes/index.tsx
    認証が完了するまでは、ローディング画面を表示するようにした。

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,22 @@ import { useRoutes } from 'react-router-dom';
 import protectedRoutes from './protected';
 import publicRoutes from './public';
 import { useAuth } from '@/hooks';
+import { Backdrop, CircularProgress } from '@mui/material';
 
 export default function AppRoutes() {
 	// 認証機能の呼び出し
-	const { isAuth } = useAuth();
+	const { isAuth, isLoading } = useAuth();
+	const routes = useRoutes(isAuth ? protectedRoutes : publicRoutes);
+
+	// 認証が完了するまでローディングを表示する。
+	if (isLoading) {
+		return (
+			<Backdrop open>
+				<CircularProgress />
+			</Backdrop>
+		);
+	}
 
 	// 認証されていたらprotectedRoutesを、されていなかったらpublicRoutesを表示する。
-	const routes = useRoutes(isAuth ? protectedRoutes : publicRoutes);
 	return <>{routes}</>;
 }
