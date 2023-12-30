744f2730334cb769f541b36123be0daa5bf879d0
natsuki <yasashisainternet@gmail.com>
2023-12-30 20:55:46 +0900

    [fix] TagLayoutのコードの形を整えた
    NoteLayoutと同じような処理をしているのにも関わらず、コードの形が違っていたので、同じ形にした。

--- src/components/Layouts/TagLayout/index.tsx
+++ src/components/Layouts/TagLayout/index.tsx
@@ -9,9 +9,12 @@ export default function TagLayout(props: TagLayoutProps) {
 	const { data } = useTagList();
 	return (
 		<ListDetailLayout
-			items={[
-				...(data?.tags.map((tag) => ({ text: tag.name, to: `/app/tag/${tag.id}` })) ?? []),
-			]}
+			items={
+				data?.tags.map((tag) => ({
+					text: tag.name,
+					to: `/app/tag/${tag.id}`,
+				})) ?? []
+			}
 		>
 			{props.children}
 		</ListDetailLayout>
