738f9fb7eef9270cfa109ebed84df7acb81a42d9
natsuki <yasashisainternet@gmail.com>
2024-01-01 00:49:56 +0900

    [update] ListItem.type.ts の型定義を追加
    ListItem.type.ts の型定義を追加して、型定義を分離ました。

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,4 @@ export type { default as Tag } from './Tag.type';
 export type { default as Author } from './Author.type';
 export type { default as NoteListItem } from './NoteListItem.type';
 export type { default as TagListItem } from './TagListItem.type';
+export type { default as ListItem } from './ListItem.type';
