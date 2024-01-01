3a5fa300eca549f6334ddcc9f66c9fc45ceb13df
natsuki <yasashisainternet@gmail.com>
2024-01-01 21:21:12 +0900

    [update] hooks/index.tsにusePostNoteを追加
    hooks/index.tsにexportを追加しました。

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 export { default as useResponsive } from './useResponsive';
 export { default as useAuth } from './useAuth';
 export { default as useNote } from './useNote';
+export { default as usePostNote } from './usePostNote';
 export { default as useNoteList } from './useNoteList';
 export { default as useTag } from './useTag';
 export { default as useTagList } from './useTagList';
