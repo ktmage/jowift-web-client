824ea1193a38fd0ba497849e142ce48fffaf325d
natsuki <yasashisainternet@gmail.com>
2024-01-01 22:07:18 +0900

    [update] NoteCreateForm.tsxを更新
    投稿機能を実装した。

--- src/components/Forms/NoteCreateForm.tsx
+++ src/components/Forms/NoteCreateForm.tsx
@@ -1,19 +1,35 @@
 import { FormLayout } from '@/components/Layouts';
+import { usePostNote, useTagList } from '@/hooks';
+import { Tag } from '@/types';
 import SaveIcon from '@mui/icons-material/Save';
 import { Autocomplete, TextField } from '@mui/material';
 import { useState } from 'react';
 
 export default function NoteCreateForm() {
 	const [title, setTitle] = useState<string>('');
-	const [tags, setTags] = useState<string[]>([]);
+	const [tags, setTags] = useState<Tag[]>([]);
 	const [content, setContent] = useState<string>('');
-	const tagOptions = ['tag1', 'tag2', 'tag3'];
+
+	const { data } = useTagList();
+	const { postNote } = usePostNote();
+
+	// valueと同じidを持つタグをTagsから探して返す。
+	const getTagsFromValues = (values: Tag[]): Tag[] => {
+		const availableTags = data?.tags ?? [];
+		return values.map((value) => availableTags.find((tag) => tag.id === value.id) || value);
+	};
+
 	return (
 		<FormLayout
 			headerItems={[
 				{
 					icon: <SaveIcon />,
-					onClick: () => console.log('save'),
+					onClick: () =>
+						postNote({
+							title,
+							content,
+							tagIds: tags.map((tag) => tag.id),
+						}),
 				},
 			]}
 		>
@@ -25,8 +41,9 @@ export default function NoteCreateForm() {
 			/>
 			<Autocomplete
 				multiple
-				options={tagOptions}
-				value={tags}
+				options={data?.tags ? data.tags : []}
+				getOptionLabel={(option) => option.name}
+				value={getTagsFromValues(tags)}
 				onChange={(e, value) => setTags(value)}
 				renderInput={(params) => (
 					<TextField
