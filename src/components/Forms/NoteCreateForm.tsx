c46cf0b6b6e43a26dcca2b9f504ce6af3bfa036f
natsuki <yasashisainternet@gmail.com>
2024-01-01 22:34:26 +0900

    [add] MultipleTagSelector.tsxの追加
    AutoCompleteを使ったタグ選択コンポーネントのロジックが煩雑化してきたので、別コンポーネントに切り出しました。

--- src/components/Forms/NoteCreateForm.tsx
+++ src/components/Forms/NoteCreateForm.tsx
@@ -1,23 +1,17 @@
 import { FormLayout } from '@/components/Layouts';
-import { usePostNote, useTagList } from '@/hooks';
+import { usePostNote } from '@/hooks';
 import { Tag } from '@/types';
 import SaveIcon from '@mui/icons-material/Save';
-import { Autocomplete, TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import { useState } from 'react';
+import { MultipleTagSelector } from '../UI';
 
 export default function NoteCreateForm() {
 	const [title, setTitle] = useState<string>('');
 	const [tags, setTags] = useState<Tag[]>([]);
 	const [content, setContent] = useState<string>('');
 
-	const { data } = useTagList();
 	const { postNote } = usePostNote();
-
-	// valueと同じidを持つタグをTagsから探して返す。
-	const getTagsFromValues = (values: Tag[]): Tag[] => {
-		const availableTags = data?.tags ?? [];
-		return values.map((value) => availableTags.find((tag) => tag.id === value.id) || value);
-	};
 
 	return (
 		<FormLayout
@@ -39,19 +33,9 @@ export default function NoteCreateForm() {
 				placeholder='タイトル'
 				onChange={(e) => setTitle(e.target.value)}
 			/>
-			<Autocomplete
-				multiple
-				options={data?.tags ? data.tags : []}
-				getOptionLabel={(option) => option.name}
-				value={getTagsFromValues(tags)}
-				onChange={(e, value) => setTags(value)}
-				renderInput={(params) => (
-					<TextField
-						{...params}
-						placeholder='タグ'
-						size='small'
-					/>
-				)}
+			<MultipleTagSelector
+				value={tags}
+				setValue={setTags}
 			/>
 			<TextField
 				multiline
