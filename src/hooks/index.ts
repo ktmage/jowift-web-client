7e8293193288dd43376b00d69c3d7c1a2cfb8d40
natsuki <yasashisainternet@gmail.com>
2024-01-01 22:40:37 +0900

    [add] usePostTag.ts
    タグを投稿するためのカスタムフックを作成しました。

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,4 +4,5 @@ export { default as useNote } from './useNote';
 export { default as usePostNote } from './usePostNote';
 export { default as useNoteList } from './useNoteList';
 export { default as useTag } from './useTag';
+export { default as usePostTag } from './usePostTag';
 export { default as useTagList } from './useTagList';
