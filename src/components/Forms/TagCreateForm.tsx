4c571c54dd30c3d8524263978f505320be0b6555
natsuki <yasashisainternet@gmail.com>
2024-01-02 22:58:02 +0900

    [update] TagCreateForm.tsxの変更
    loadingのフラグが立っている間は操作を受け付けないようにした。

--- src/components/Forms/TagCreateForm.tsx
+++ src/components/Forms/TagCreateForm.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { Backdrop, CircularProgress, TextField } from '@mui/material';
 import { FormLayout } from '../Layouts';
 import SaveIcon from '@mui/icons-material/Save';
 import { useState } from 'react';
@@ -6,22 +6,31 @@ import { usePostTag } from '@/hooks';
 
 export default function TagCreateForm() {
 	const [name, setName] = useState<string>('');
-	const { postTag } = usePostTag();
+	const { postTag, isLoading } = usePostTag();
 	return (
-		<FormLayout
-			headerItems={[
-				{
-					icon: <SaveIcon />,
-					onClick: () => postTag(name),
-				},
-			]}
-		>
-			<TextField
-				inputProps={{ sx: { fontSize: '1.5rem', fontWeight: 'bold' } }}
-				value={name}
-				placeholder='タグの名前'
-				onChange={(e) => setName(e.target.value)}
-			/>
-		</FormLayout>
+		<>
+			<Backdrop open={isLoading}>
+				<CircularProgress />
+			</Backdrop>
+			<FormLayout
+				headerItems={[
+					{
+						icon: <SaveIcon />,
+						onClick: () => postTag(name),
+						disabled: isLoading,
+					},
+				]}
+			>
+				<TextField
+					inputProps={{
+						sx: { fontSize: '1.5rem', fontWeight: 'bold' },
+						readOnly: isLoading,
+					}}
+					value={name}
+					placeholder='タグの名前'
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</FormLayout>
+		</>
 	);
 }
