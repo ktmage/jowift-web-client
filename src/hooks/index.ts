480095ebcb744bef21ac8b77f5695ddf8c23d5de
natsuki <yasashisainternet@gmail.com>
2024-01-04 23:20:08 +0900

    [add] usePutTag.tsの追加
    Tagを更新するためのカスタムフックを追加しました。

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -6,5 +6,6 @@ export { default as usePutNote } from './usePutNote';
 export { default as useNoteList } from './useNoteList';
 export { default as useTag } from './useTag';
 export { default as usePostTag } from './usePostTag';
+export { default as usePutTag } from './usePutTag';
 export { default as useTagList } from './useTagList';
 export { default as useNotification } from './useNotification';
