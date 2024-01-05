ce9aef35aaf0b16b85d2224df0a2ffae7923a35a
natsuki <yasashisainternet@gmail.com>
2024-01-05 10:56:26 +0900

    [add] useDeleteNote.tsを追加
    Noteを削除するためのカスタムフックを追加しました。

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,15 @@
 export { default as useResponsive } from './useResponsive';
+export { default as useNotification } from './useNotification';
+
 export { default as useAuth } from './useAuth';
+
 export { default as useNote } from './useNote';
 export { default as usePostNote } from './usePostNote';
 export { default as usePutNote } from './usePutNote';
+export { default as useDeleteNote } from './useDeleteNote';
 export { default as useNoteList } from './useNoteList';
+
 export { default as useTag } from './useTag';
 export { default as usePostTag } from './usePostTag';
 export { default as usePutTag } from './usePutTag';
 export { default as useTagList } from './useTagList';
-export { default as useNotification } from './useNotification';
