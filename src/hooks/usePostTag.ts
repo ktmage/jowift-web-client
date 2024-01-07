725b8df86d8059107cf78e547600303166b3cd95
natsuki <yasashisainternet@gmail.com>
2024-01-07 22:59:03 +0900

    [update] usePostTag.tsの修正
    repositoryを使った簡潔な記述に変更しました。

--- src/hooks/usePostTag.ts
+++ src/hooks/usePostTag.ts
@@ -1,53 +1,38 @@
+import { Tag } from '@/models';
+import { TagRepository } from '@/repositories';
 import { useState } from 'react';
-import { API_URL } from '@/config';
 import useNotification from './useNotification';
 
 export default function usePostTag() {
+	const [tag, setTag] = useState<Tag | null>(null);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const [error, setError] = useState<string | null>(null);
-
+	const [error, setError] = useState<Error | null>(null);
+	const tagRepository = new TagRepository();
 	const { dispatchNotification } = useNotification();
 
 	const postTag = async (name: string) => {
 		setIsLoading(true);
-		setError(null);
-
 		try {
-			const response = await fetch(API_URL + '/tag', {
-				method: 'POST',
-				mode: 'cors',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					name,
-				}),
-			});
-
-			if (!response.ok) {
+			const result = await tagRepository.post(name);
+			setTag(result);
+			setError(null);
+		} catch (e) {
+			setError(e as Error);
+		} finally {
+			if (error) {
 				dispatchNotification({
 					severity: 'error',
 					message: '送信に失敗しました。',
 				});
-				throw new Error('Failed to post tag');
-			}
-			dispatchNotification({
-				severity: 'success',
-				message: '送信に成功しました。',
-			});
-			const result = await response.json();
-			return result;
-		} catch (err) {
-			if (err instanceof Error) {
-				setError(err.message);
 			} else {
-				setError('An unknown error occurred');
+				dispatchNotification({
+					severity: 'success',
+					message: '送信に成功しました。',
+				});
 			}
-		} finally {
 			setIsLoading(false);
 		}
 	};
 
-	return { postTag, isLoading, error };
+	return { postTag, tag, isLoading, error };
 }
