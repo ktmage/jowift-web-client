dbd8e37b6964393cbfa25abee4c60a916c1fbeeb
natsuki <yasashisainternet@gmail.com>
2024-01-08 03:09:51 +0900

    [update] useNoteList.tsを修正
    NoteRepository.getAllを使って処理を簡潔にしました。

--- src/hooks/usecase/note/useNoteList.ts
+++ src/hooks/usecase/note/useNoteList.ts
@@ -1,23 +1,16 @@
-import { API_URL, SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES } from '@/config';
+import { SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES } from '@/config';
+import { Note } from '@/models';
+import { NoteRepository } from '@/repositories';
+import CacheKeyGenerator from '@/util/CacheKeyGenerator';
 import useSWR from 'swr';
-import { NoteListItem } from '@/types';
 
 export default function useNoteList() {
-	const fetcher = (url: string) =>
-		fetch(url, {
-			method: 'GET',
-			mode: 'cors',
-			credentials: 'include',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		}).then((res) => res.json());
+	const repository = new NoteRepository();
 
-	const { data, isLoading, error, mutate } = useSWR<{ notes: NoteListItem[] }>(
-		API_URL + '/note',
-		fetcher,
+	const { data, isLoading, error, mutate } = useSWR<Note[]>(
+		CacheKeyGenerator.generateNoteListKey(),
+		() => repository.getAll(),
 		{
-			// 一定間隔でデータを同期
 			refreshInterval: 1000 * 60 * SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES,
 			revalidateIfStale: false,
 		},
@@ -25,3 +18,31 @@ export default function useNoteList() {
 
 	return { data, isLoading, error, mutate };
 }
+
+// import { API_URL, SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES } from '@/config';
+// import useSWR from 'swr';
+// import { NoteListItem } from '@/types';
+
+// export default function useNoteList() {
+// 	const fetcher = (url: string) =>
+// 		fetch(url, {
+// 			method: 'GET',
+// 			mode: 'cors',
+// 			credentials: 'include',
+// 			headers: {
+// 				'Content-Type': 'application/json',
+// 			},
+// 		}).then((res) => res.json());
+
+// 	const { data, isLoading, error, mutate } = useSWR<{ notes: NoteListItem[] }>(
+// 		API_URL + '/note',
+// 		fetcher,
+// 		{
+// 			// 一定間隔でデータを同期
+// 			refreshInterval: 1000 * 60 * SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES,
+// 			revalidateIfStale: false,
+// 		},
+// 	);
+
+// 	return { data, isLoading, error, mutate };
+// }
