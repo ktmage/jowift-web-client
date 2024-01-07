7e37f230f04b5dca43acef3bb18e438f2cae5bdf
natsuki <yasashisainternet@gmail.com>
2024-01-08 03:43:50 +0900

    [update] usePutNoteを更新
    NoteRepositoryを使って簡潔なコードに変更しました。

--- src/hooks/usecase/note/usePutNote.ts
+++ src/hooks/usecase/note/usePutNote.ts
@@ -1,62 +1,53 @@
+import { NoteRepository } from '@/repositories';
 import { useState } from 'react';
-import { API_URL } from '@/config';
 import useNotification from '../../useNotification';
+import useNote from './useNote';
+import useNoteList from './useNoteList';
+import { Tag } from '@/models';
 
-export default function usePutNote() {
+export default function usePutNote(id: string) {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const [error, setError] = useState<string | null>(null);
-
+	const [error, setError] = useState<Error | null>(null);
+	const noteRepository = new NoteRepository();
 	const { dispatchNotification } = useNotification();
 
-	const putNote = async (
-		{
-			title,
-			content,
-			tagIds,
-		}: {
-			title: string;
-			content: string;
-			tagIds: string[];
-		},
-		id: string,
-	) => {
+	const { mutate: mutateNote } = useNote(id);
+	const { mutate: mutateNoteList } = useNoteList();
+
+	const effect = async () => {
+		await mutateNote();
+		await mutateNoteList();
+	};
+
+	const putNote = async ({
+		title,
+		content,
+		tags,
+	}: {
+		title: string;
+		content: string;
+		tags: Tag[];
+	}) => {
 		setIsLoading(true);
-		setError(null);
-
 		try {
-			const response = await fetch(API_URL + '/note/' + id, {
-				method: 'PUT',
-				mode: 'cors',
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					title,
-					content,
-					tagId: tagIds,
-				}),
-			});
-
-			if (!response.ok) {
-				dispatchNotification({
-					severity: 'error',
-					message: '更新に失敗しました。',
-				});
-				throw new Error('Failed to post note');
-			}
+			await noteRepository.put(
+				id,
+				title,
+				content,
+				tags.map((tag: Tag) => tag.id),
+			);
+			setError(null);
+			await effect();
 			dispatchNotification({
 				severity: 'success',
-				message: '更新に成功しました。',
+				message: '送信に成功しました。',
 			});
-			const result = await response.json();
-			return result;
-		} catch (err) {
-			if (err instanceof Error) {
-				setError(err.message);
-			} else {
-				setError('An unknown error occurred');
-			}
+		} catch (e) {
+			dispatchNotification({
+				severity: 'error',
+				message: '送信に失敗しました。',
+			});
+			setError(e as Error);
 		} finally {
 			setIsLoading(false);
 		}
