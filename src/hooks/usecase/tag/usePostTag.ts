f21ac131f18f5e249c8ef654780c33b0abd06338
natsuki <yasashisainternet@gmail.com>
2024-01-08 01:56:38 +0900

    [update] usePostTag.tsに副作用を追加
    タグの投稿に副作用を追加しました。

--- src/hooks/usecase/tag/usePostTag.ts
+++ src/hooks/usecase/tag/usePostTag.ts
@@ -2,6 +2,8 @@ import { Tag } from '@/models';
 import { TagRepository } from '@/repositories';
 import { useState } from 'react';
 import useNotification from '../../useNotification';
+import useTagList from './useTagList';
+import { useNavigate } from 'react-router-dom';
 
 export default function usePostTag() {
 	const [tag, setTag] = useState<Tag | null>(null);
@@ -10,17 +12,28 @@ export default function usePostTag() {
 	const tagRepository = new TagRepository();
 	const { dispatchNotification } = useNotification();
 
+	const { mutate: mutateTagList } = useTagList();
+	const Navigate = useNavigate();
+
+	const effect = async (id: string) => {
+		await mutateTagList();
+		Navigate(`/app/tag/${id}`);
+	};
+
 	const postTag = async (name: string) => {
 		setIsLoading(true);
 		try {
 			const result = await tagRepository.post(name);
 			setTag(result);
 			setError(null);
+			console.log(result);
+			await effect(result.id);
 			dispatchNotification({
 				severity: 'success',
 				message: '送信に成功しました。',
 			});
 		} catch (e) {
+			console.log(e);
 			dispatchNotification({
 				severity: 'error',
 				message: '送信に失敗しました。',
