9d58840e19e424f8225a581ae97d7dd84cfe84ee
natsuki <yasashisainternet@gmail.com>
2024-01-07 22:57:05 +0900

    [update] TagRepository.postの追加
    タグを投稿をするためのメソッドを追加しました。

--- src/repositories/TagRepository.ts
+++ src/repositories/TagRepository.ts
@@ -33,4 +33,21 @@ export default class TagRepository {
 		const json = await response.json();
 		return json.tags.map((tag: Tag) => new Tag(tag.id, tag.name));
 	}
+
+	async post(name: string): Promise<Tag> {
+		const response = await fetch(API_URL + '/tag', {
+			method: 'POST',
+			mode: 'cors',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ name }),
+		});
+		if (!response.ok) {
+			throw new Error('Failed to fetch');
+		}
+		const json = await response.json();
+		return new Tag(json.tag.id, json.tag.name);
+	}
 }
