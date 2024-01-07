e1f2c545704e454795502b8702bc35bb1f307a0a
natsuki <yasashisainternet@gmail.com>
2024-01-08 03:12:07 +0900

    [delete] useNoteList.tsのコメントを削除しました。
    コメントを消し忘れていたので削除しました。

--- src/hooks/usecase/note/useNoteList.ts
+++ src/hooks/usecase/note/useNoteList.ts
@@ -18,31 +18,3 @@ export default function useNoteList() {
 
 	return { data, isLoading, error, mutate };
 }
-
-// import { API_URL, SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES } from '@/config';
-// import useSWR from 'swr';
-// import { NoteListItem } from '@/types';
-
-// export default function useNoteList() {
-// 	const fetcher = (url: string) =>
-// 		fetch(url, {
-// 			method: 'GET',
-// 			mode: 'cors',
-// 			credentials: 'include',
-// 			headers: {
-// 				'Content-Type': 'application/json',
-// 			},
-// 		}).then((res) => res.json());
-
-// 	const { data, isLoading, error, mutate } = useSWR<{ notes: NoteListItem[] }>(
-// 		API_URL + '/note',
-// 		fetcher,
-// 		{
-// 			// 一定間隔でデータを同期
-// 			refreshInterval: 1000 * 60 * SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES,
-// 			revalidateIfStale: false,
-// 		},
-// 	);
-
-// 	return { data, isLoading, error, mutate };
-// }
