e4f9dfd11688b33e33c104c42d958e27c022f8bf
natsuki <yasashisainternet@gmail.com>
2024-01-08 23:27:29 +0900

    [add] SettingForm.tsxを追加
    SettingForm.tsxを追加しました

--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -2,3 +2,4 @@ export { default as NoteCreateForm } from './NoteCreateForm';
 export { default as NoteDetailForm } from './NoteDetailForm';
 export { default as TagCreateForm } from './TagCreateForm';
 export { default as TagDetailForm } from './TagDetailForm';
+export { default as SettingForm } from './SettingForm';
