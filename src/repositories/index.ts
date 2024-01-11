06e4a03b48d172d97859025f5f77c8a7d27a1589
natsuki <yasashisainternet@gmail.com>
2024-01-11 23:36:28 +0900

    [fix] repositories/index.ts
    settingrepositoryのimportを修正

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,3 +1,2 @@
 export { default as NoteRepository } from './NoteRepository';
 export { default as TagRepository } from './TagRepository';
-export { default as SettingRepository } from './SettingRepository';
