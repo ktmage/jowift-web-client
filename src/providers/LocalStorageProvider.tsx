a2d6f5bcb2589c6beb27d0b97d6b8e9827567a0c
natsuki <yasashisainternet@gmail.com>
2024-01-13 17:55:35 +0900

    [update] LocalStorageProvider.tsx
    ローカルストレージの値を保持するProviderに変更しました。

---
+++ src/providers/LocalStorageProvider.tsx
@@ -0,0 +1,64 @@
+import { useLocalStorage } from '@/hooks';
+import { ReactNode, createContext } from 'react';
+
+interface SettingContextProps {
+	theme: {
+		value: 'light' | 'dark';
+		setValue: (value: 'light' | 'dark') => void;
+	};
+	language: {
+		value: 'en' | 'jp';
+		setValue: (value: 'en' | 'jp') => void;
+	};
+	isHappiness: {
+		value: boolean;
+		setValue: (value: boolean) => void;
+	};
+}
+
+export const SettingContext = createContext({} as SettingContextProps);
+
+export const SettingProvider = ({ children }: { children: ReactNode }) => {
+	const { value: theme, setValue: setTheme } = useLocalStorage<'light' | 'dark'>(
+		'theme',
+		'light',
+		(value) => {
+			return value === 'light' || value === 'dark';
+		},
+	);
+	const { value: language, setValue: setLanguage } = useLocalStorage<'en' | 'jp'>(
+		'language',
+		'en',
+		(value) => {
+			return value === 'en' || value === 'jp';
+		},
+	);
+	const { value: isHappiness, setValue: setIsHappiness } = useLocalStorage<boolean>(
+		'isHappiness',
+		false,
+		(value) => {
+			return typeof value === 'boolean';
+		},
+	);
+
+	return (
+		<SettingContext.Provider
+			value={{
+				theme: {
+					value: theme,
+					setValue: setTheme,
+				},
+				language: {
+					value: language,
+					setValue: setLanguage,
+				},
+				isHappiness: {
+					value: isHappiness,
+					setValue: setIsHappiness,
+				},
+			}}
+		>
+			{children}
+		</SettingContext.Provider>
+	);
+};
