dc1a914afa1f4210b45ad3224b6b6cb6850dc37e
natsuki <yasashisainternet@gmail.com>
2024-01-14 23:49:04 +0900

    [add] UserRepository.tsを追加
    サーバーとUserに関する通信を行う処理を追加しました。

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,50 @@
+import { API_URL } from '@/config';
+
+interface User {
+	name: string;
+	email: string;
+}
+
+interface IUserRepository {
+	get: () => Promise<User>;
+	delete: () => Promise<boolean>;
+}
+
+const UserRepository: IUserRepository = {
+	get: async (): Promise<User> => {
+		const response = await fetch(API_URL + '/auth/user', {
+			method: 'GET',
+			mode: 'cors',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		if (!response.ok) {
+			throw new Error('Failed to fetch');
+		}
+		const json = await response.json();
+		const user: User = {
+			name: json.data.username,
+			email: json.data.email,
+		};
+		return user;
+	},
+	delete: async (): Promise<boolean> => {
+		const response = await fetch(API_URL + '/auth/user', {
+			method: 'DELETE',
+			mode: 'cors',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		if (!response.ok) {
+			throw new Error('Failed to fetch');
+		}
+
+		return true;
+	},
+};
+
+export default UserRepository;
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,3 @@
 export { default as NoteRepository } from './NoteRepository';
 export { default as TagRepository } from './TagRepository';
+export { default as UserRepository } from './UserRepository';
