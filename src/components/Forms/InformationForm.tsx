121211b5500f83bb984b534382e05127ae81677e
natsuki <yasashisainternet@gmail.com>
2024-01-15 00:24:40 +0900

    [update] InformationFormを変更
    BackdropとFormLayoutを追加

--- src/components/Forms/InformationForm.tsx
+++ src/components/Forms/InformationForm.tsx
@@ -1,3 +1,18 @@
+import { Backdrop, CircularProgress } from '@mui/material';
+import { FormLayout } from '../Layouts';
+
 export default function InformationForm() {
-	return <>InformationForm</>;
+	return (
+		<>
+			<Backdrop open={false}>
+				<CircularProgress />
+			</Backdrop>
+			<FormLayout
+				disableHeader
+				headerItems={[]}
+			>
+				information form
+			</FormLayout>
+		</>
+	);
 }
