6cfa4d02be6c1632c9ff3aff16b76bf3e553ee69
natsuki <yasashisainternet@gmail.com>
2024-01-15 00:20:15 +0900

    [add] InformationForm.tsx
    information用のフォームを作成しました。

--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -4,3 +4,4 @@ export { default as TagCreateForm } from './TagCreateForm';
 export { default as TagDetailForm } from './TagDetailForm';
 export { default as SettingForm } from './SettingForm';
 export { default as AccountForm } from './AccountForm';
+export { default as InformationForm } from './InformationForm';
