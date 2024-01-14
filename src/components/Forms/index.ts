0e2e24a5f711ff2dca4603a565c3321a959e3d8a
natsuki <yasashisainternet@gmail.com>
2024-01-14 20:31:54 +0900

    [add] AccountForm.tsxを追加
    Account用のFormコンポーネントを追加しました。

--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -3,3 +3,4 @@ export { default as NoteDetailForm } from './NoteDetailForm';
 export { default as TagCreateForm } from './TagCreateForm';
 export { default as TagDetailForm } from './TagDetailForm';
 export { default as SettingForm } from './SettingForm';
+export { default as AccountForm } from './AccountForm';
--- src/routes/elements/Account.tsx
+++ src/routes/elements/Account.tsx
@@ -1,9 +1,10 @@
+import { AccountForm } from '@/components/Forms';
 import { AccountLayout } from '@/components/Layouts';
 
 export default function Account() {
 	return (
 		<AccountLayout>
-			<>account</>
+			<AccountForm />
 		</AccountLayout>
 	);
 }
