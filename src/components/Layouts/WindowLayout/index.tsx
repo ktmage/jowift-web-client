9b9ef1ff3a46f191bab7bca33c1e6efac919c806
natsuki <yasashisainternet@gmail.com>
2024-01-22 22:50:31 +0900

    [add] 新たなレイアウトを追加
    WindowLayoutコンポーネントを追加しました。

---
+++ src/components/Layouts/WindowLayout/index.tsx
@@ -0,0 +1,32 @@
+import { useResponsive } from '@/hooks';
+import { Box, Paper } from '@mui/material';
+
+interface WindowLayoutProps {
+	children: React.ReactNode;
+}
+
+export default function WindowLayout(props: WindowLayoutProps) {
+	const { isDesktop } = useResponsive();
+
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				height: '100vh',
+				width: '100vw',
+			}}
+		>
+			<Paper
+				sx={{
+					width: isDesktop ? '50%' : '90%',
+					bgcolor: 'background.paper',
+					p: 2,
+				}}
+			>
+				{props.children}
+			</Paper>
+		</Box>
+	);
+}
