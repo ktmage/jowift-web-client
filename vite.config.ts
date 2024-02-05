cc58f48fc621a065f26d91df569a0625d9aa89d5
natsuki <yasashisainternet@gmail.com>
2024-02-06 01:54:48 +0900

    [update] vite.config.tsにpwaの設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+			injectRegister: 'auto',
+			manifest: {
+				name: 'Jowift',
+				short_name: 'Jowift',
+				description: 'Stylish memo application.',
+				theme_color: '#57b957',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 	server: {
 		port: 3000,
 	},
