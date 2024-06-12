e6815febeb7e02d448121b895909ce415427a749
ktmage <yasashisainternet@gmail.com>
2024-06-13 06:49:16 +0900

    [update] : useMutation.tsにoptionsを追加
    処理のライフサイクルに合わせたコールバック関数を追加しました。

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -1,8 +1,12 @@
 // src/hooks/useMutation.ts
 import { useState } from 'react';
 import useNotification from './useNotification';
+import { MutationOptions } from '@/types';
 
-export default function useMutation<T>(mutationFunction: () => Promise<T>) {
+export default function useMutation<T>(
+	mutationFunction: () => Promise<T>,
+	options: MutationOptions<T> = {},
+) {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState<Error | null>(null);
 	const [data, setData] = useState<T | null>(null);
@@ -10,12 +14,16 @@ export default function useMutation<T>(mutationFunction: () => Promise<T>) {
 
 	const mutate = async () => {
 		setIsLoading(true);
+		options.onStart?.();
+
 		try {
-			setData(await mutationFunction());
+			const result = await mutationFunction();
+			setData(result);
 			setError(null);
 			dispatchNotification({
 				severity: 'success',
 			});
+			options.onSuccess?.(result);
 		} catch (error) {
 			if (error instanceof Error) {
 				setError(error);
@@ -23,9 +31,11 @@ export default function useMutation<T>(mutationFunction: () => Promise<T>) {
 					severity: 'error',
 					message: error.message,
 				});
+				options.onError?.(error);
 			}
 		} finally {
 			setIsLoading(false);
+			options.onSettled?.(data, error);
 		}
 	};
 
