d3c73abc7224e3e5fd7d18bef953d224ab0fe3e7
ktmage <yasashisainternet@gmail.com>
2024-06-13 06:53:54 +0900

    [update] : types/indexにMutationOptionsを追加

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export type { default as ListItem } from './ListItem.type';
 export type { default as ListHeaderItem } from './ListHeaderItem.type';
+export type { default as MutationOptions } from './MutationOptions.type';
