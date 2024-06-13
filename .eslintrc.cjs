5d53ec1d04df398aa27dfefec92ccebcf23152d6
ktmage <yasashisainternet@gmail.com>
2024-06-13 13:03:48 +0900

    [update] : eslintの設定を変更

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,11 +9,61 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: "variable",
+        format: ["camelCase"],
+      },
+      {
+        selector: "function",
+        format: ["camelCase", "PascalCase"],
+      },
+      {
+        selector: "parameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "class",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "method",
+        format: ["camelCase"],
+      },
+      {
+        selector: "property",
+        format: ["camelCase"],
+      },
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeAlias",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeParameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "enum",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "enumMember",
+        format: ["UPPER_CASE"],
+      },
     ],
   },
 }
