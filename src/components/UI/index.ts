8c13bd8bf3b73392dfcd36dddc4c49e8fcd7e5be
ktmage <yasashisainternet@gmail.com>
2024-06-15 21:49:00 +0900

    [delete] : src/componentをすべてを破壊する。東京の摩天楼を手でかき分けながら何かを探しているようだった。それは忘れかけた人間性か、私か。鉄筋コンクリートの壁を砕き、地に轟く足音。燃え上がる景色とは対称的に消え入りそうな小さな炎。怪獣の瞳からは、ちっぽけな私では到底受け止められないような涙が零れ落ち、声の出ぬ間に蒸発した。

--- src/components/UI/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as MultipleTagSelector } from './MultipleTagSelector/MultipleTagSelector';
-export { default as LogoSvg } from './LogoSvg/LogoSvg';
--- src/components/forms/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as NoteCreateForm } from './NoteCreateForm/NoteCreateForm';
-export { default as NoteDetailForm } from './NoteDetailForm/NoteDetailForm';
-export { default as TagCreateForm } from './TagCreateForm/TagCreateForm';
-export { default as TagDetailForm } from './TagDetailForm/TagDetailForm';
-export { default as SettingForm } from './SettingForm/SettingForm';
-export { default as AccountForm } from './AccountForm/AccountForm';
-export { default as InformationForm } from './InformationForm/InformationForm';
-export { default as AuthForm } from './AuthForm/AuthForm';
