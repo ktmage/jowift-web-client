603f1a63a6dc9e915f5052a34b1ad19595e77a25
ktmage <yasashisainternet@gmail.com>
2024-07-13 07:34:55 +0900

    [update] : ListViewのディレクトリを変更
    再利用性が低いため、MainPage内に再配置しました。

--- src/components/pages/MainPage/ListView/ListView.tsx
+++ src/components/pages/MainPage/ListView/ListView.tsx
@@ -1,13 +1,21 @@
-import { Box, Divider, IconButton, List, ListItemButton, Tooltip, Typography } from '@mui/material';
+import { Box, Divider, List, ListItemButton, Tooltip, Typography } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { ListHeaderItem } from '@/types';
-import { useNoteList } from '@/hooks';
+import { useNoteList, useResponsive } from '@/hooks';
 import AddIcon from '@mui/icons-material/Add';
+import IconButton from '@/components/ui/IconButton/IconButton';
+import DehazeIcon from '@mui/icons-material/Dehaze';
 
-export default function ListView() {
+type ListViewProps = {
+	toggleSplitRatio: () => void;
+};
+
+export default function ListView(props: ListViewProps) {
 	const navigate = useNavigate();
 
 	const { noteList } = useNoteList();
+
+	const { isMobile } = useResponsive();
 
 	const headerItems: ListHeaderItem[] = [
 		{
@@ -55,10 +63,22 @@ export default function ListView() {
 						key={index}
 						onClick={item.onClick}
 						disabled={item.disabled}
+						variant='ghost'
+						size='small'
 					>
 						{item.icon}
 					</IconButton>
 				))}
+				{/* Mobileだったら追加でIconButtonを表示 */}
+				{isMobile && (
+					<IconButton
+						onClick={props.toggleSplitRatio}
+						variant='ghost'
+						size='small'
+					>
+						<DehazeIcon />
+					</IconButton>
+				)}
 			</Box>
 			<Divider />
 			<Box
