6d1b9ee76beea3b00443435063051401bb053032
ktmage <yasashisainternet@gmail.com>
2024-07-20 02:24:28 +0900

    [delete] : MultipleTagSelector.tsxを削除

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,2 @@
-export { default as MultipleTagSelector } from './MultipleTagSelector/MultipleTagSelector';
 export { default as LogoSvg } from './LogoSvg/LogoSvg';
 export { default as Backdrop } from './Backdrop/Backdrop';
