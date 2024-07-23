cd2edcb62bbc0c7f0e2b2a19f24f5d646eeac5d9
ktmage <yasashisainternet@gmail.com>
2024-07-23 23:48:17 +0900

    [update] : Sidebarに検索機能を追加

--- src/components/pages/MainPage/Sidebar/Sidebar.tsx
+++ src/components/pages/MainPage/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { useNoteList } from '@/hooks';
+import { useNoteList, useTagList } from '@/hooks';
 
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -11,9 +11,11 @@ import SidebarSection from './SidebarSection/SidebarSection';
 import SidebarHeader from './SidebarHeader/SidebarHeader';
 import SidebarSectionLink from './SidebarSectionLink/SidebarSectionLink';
 import SidebarSectionButton from './SidebarSectionButton/SidebarSectionButton';
+import MultipleSelector from '@/components/elements/MultipleSelector/MultipleSelector';
 
 export default function Sidebar() {
-	const { noteList } = useNoteList();
+	const { noteList, setFilterTags, filterTags } = useNoteList();
+	const { tagList } = useTagList();
 
 	return (
 		<div className='h-full flex flex-col'>
@@ -31,11 +33,36 @@ export default function Sidebar() {
 					/>
 					<SidebarSectionButton
 						onClick={() => {
-							console.log('search');
+							const modal = document.querySelector('.modal') as HTMLDialogElement;
+							modal.showModal();
 						}}
 						label='検索'
 						icon={<SearchIcon />}
 					/>
+
+					<dialog className='modal'>
+						<div className='modal-box flex flex-col gap-4'>
+							<h3 className='font-bold text-lg'>検索</h3>
+							<MultipleSelector
+								options={tagList?.map((tag) => tag.name) || []}
+								value={filterTags.map((tag) => tag.name)}
+								placeholder={'タグを選択'}
+								onChange={function (newValue: string[]): void {
+									const updatedTags =
+										tagList?.filter((tag) => newValue.includes(tag.name)) || [];
+									setFilterTags(updatedTags);
+								}}
+								alwaysOpen={true}
+								noElevation={true}
+							/>
+							<div className='modal-action'>
+								<form method='dialog'>
+									<button className='btn'>閉じる</button>
+								</form>
+							</div>
+						</div>
+					</dialog>
+
 					<SidebarSectionLink
 						to='/account'
 						label='アカウント'
