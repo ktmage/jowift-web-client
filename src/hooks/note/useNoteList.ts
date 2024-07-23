10de214d169d8729187a5c60358fe0fdf0a5497c
ktmage <yasashisainternet@gmail.com>
2024-07-23 23:46:18 +0900

    [update] : useNoteListにタグによるフィルタリング機能を追加

--- src/hooks/note/useNoteList.ts
+++ src/hooks/note/useNoteList.ts
@@ -1,8 +1,9 @@
 import { SWR_NOTE_LIST_REFRESH_INTERVAL_MINUTES } from '@/config';
-import { NoteModel } from '@/models';
+import { NoteModel, TagModel } from '@/models';
 import { NoteRepository } from '@/repositories';
 import { CacheKeyGenerator } from '@/utilities';
 import { useDataFetcher } from '@/hooks';
+import { useEffect, useState } from 'react';
 
 export default function useNoteList() {
 	const repository = new NoteRepository();
@@ -15,5 +16,24 @@ export default function useNoteList() {
 		},
 	);
 
-	return { noteList: data, isLoading, error, mutate };
+	const [filteredNotes, setFilteredNotes] = useState<NoteModel[]>([]);
+	const [filterTags, setFilterTags] = useState<TagModel[]>([]);
+
+	// 特定のタグでフィルタリングする
+	useEffect(() => {
+		if (data) {
+			if (filterTags.length === 0) {
+				setFilteredNotes(data);
+			} else {
+				const filtered = data.filter((note) => {
+					return filterTags.every((tag) =>
+						note.tags.some((noteTag) => noteTag.id === tag.id),
+					);
+				});
+				setFilteredNotes(filtered);
+			}
+		}
+	}, [data, filterTags]);
+
+	return { noteList: filteredNotes, isLoading, error, mutate, filterTags, setFilterTags };
 }
